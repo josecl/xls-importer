b22bc8b7789cc0e523a3572e623d3afed5911df4
Jose Rodriguez <jose.rodriguez@zecovery.com>
2022-09-28 11:04:28 +0200

    test: naming

--- tests/XlsImporterTest.php
+++ tests/XlsImporterTest.php
@@ -9,7 +9,7 @@ use Josecl\XlsImporter\OpenSpoutXlsxWriter;
 use Josecl\XlsImporter\SheetNotFoundException;
 use Josecl\XlsImporter\XlsImporter;
 
-test('Export', closure: function (string $filename, mixed $destination) {
+test('Export providing Writer', closure: function (string $filename, mixed $destination) {
     $importer = new XlsImporter(__DIR__ . '/spreadsheet.xls');
     $importer->import('Sheet 1', $destination);
 
@@ -19,20 +19,20 @@ test('Export', closure: function (string $filename, mixed $destination) {
     //                                  output                   input
     //                                 =======================   ==============================================
     'xlsx string' => /*             */ [__DIR__ . '/out.Xlsx',   __DIR__ . '/out.Xlsx'],
-    'xlsx OpenSpoutXlsxWriter' /**/ => [__DIR__ . '/out.Xlsx',   new OpenSpoutXlsxWriter(__DIR__ . '/out.Xlsx')],
+    'xlsx OpenSpoutXlsxWriter' => /**/ [__DIR__ . '/out.Xlsx',   new OpenSpoutXlsxWriter(__DIR__ . '/out.Xlsx')],
     'csv string' =>  /*             */ [__DIR__ . '/out.csv',    __DIR__ . '/out.csv'],
     'csv OpenSpoutCsvWriter' => /*  */ [__DIR__ . '/out.csv',    new OpenSpoutCsvWriter(__DIR__ . '/out.csv')],
 ]);
 
 
-test('Invalid extension', closure: function () {
+test('InvalidExtensionException', closure: function () {
     $importer = new XlsImporter(__DIR__ . '/spreadsheet.xls');
     $this->expectException(InvalidExtensionException::class);
     $importer->import('Sheet 1', __DIR__ . '/out.InValId');
 });
 
 
-test('Invalid sheet name', closure: function () {
+test('SheetNotFoundException', closure: function () {
     $importer = new XlsImporter(__DIR__ . '/spreadsheet.xls');
     $this->expectException(SheetNotFoundException::class);
     $importer->import('Invalid Sheet name', __DIR__ . '/out.csv');
